fb707255d6f739d25af81483ba456246b18757db
YifanXu <yifanxu00@outlook.com>
2022-11-16 16:17:09 -0800

    fix typo

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -21,7 +21,7 @@ function Root() {
               height="30"
               className="d-inline-block align-top"
             />{' '}
-            <NavLink className="navLink brand" as={Link} to="/">Terra Invicta Ship6builder</NavLink>
+            <NavLink className="navLink brand" as={Link} to="/">Terra Invicta Shipbuilder</NavLink>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
